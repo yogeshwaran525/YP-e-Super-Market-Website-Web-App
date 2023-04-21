de8c2b22ae81c7575497e386d94582f8a983aec4
Yogesh Waran P <118171405+yogeshwaran525@users.noreply.github.com>
2023-04-21 20:25:57 +0530

    Update page.js

--- pages/page.js
+++ pages/page.js
@@ -1,84 +1,54 @@
 const express = require('express');
 const router = express.Router();
 const controller = require("../controller/purchase");
+router.get('/',(req,res)=>{ res.render('home')})
+router.get('/login',(req,res)=>{ res.render('login')})
+router.get("/logout", controller.logout);
 
-router.get('/',(req,res)=>{
-    res.render('home')
-})
+router.get('/createaccount',(req,res)=>{ res.render('createaccount')})
 
-router.get('/login',(req,res)=>{
-    res.render('login')
-})
+router.get('/icecream',(req,res)=>{ res.render('icecream')})
 
+router.get('/addtocart',(req,res)=>{ res.render('addtocart')})
 
-router.get('/createaccount',(req,res)=>{
-    res.render('createaccount')
-})
+router.get('/fruit',(req,res)=>{ res.render('fruit')})
 
-router.get('/icecream',(req,res)=>{
-    res.render('icecream')
-})
+router.get('/vegetable',(req,res)=>{ res.render('vegetables')})
 
-router.get('/addtocart',(req,res)=>{
-    res.render('addtocart')
-})
+router.get('/snack',(req,res)=>{ res.render('snack')})
 
-router.get('/fruit',(req,res)=>{
-    res.render('fruit')
-})
+router.get('/dailyessentials',(req,res)=>{res.render('dailyessentials')})
 
-router.get('/vegetables',(req,res)=>{
-    res.render('vegetables')
-})
+router.get('/juice',(req,res)=>{res.render('juice')})
 
-router.get('/snack',(req,res)=>{
-    res.render('snack')
-})
+router.get('/meat',(req,res)=>{ res.render('meat')})
 
-router.get('/dailyessentials',(req,res)=>{
-    res.render('dailyessentials')
-})
+router.get('/purchase',(req,res)=>{ res.render('purchase')});
 
-router.get('/juice',(req,res)=>{
-    res.render('juice')
-})
+router.get('/thanks',controller.profile,(req,res)=>{ res.render('thanks',
+{ data: req.data })});
 
-router.get('/meat',(req,res)=>{
-    res.render('meat')
-})
+router.get('/regsuccess',(req,res)=>{  res.render('regsuccess')}) 
 
-router.get('/purchase',(req,res)=>{
-    res.render('purchase')
+router.get('/regfailed',(req,res)=>{ res.render('regfailed')}) 
+
+router.get('/loginfailed',(req,res)=>{ res.render('loginfailed')}) 
+
+router.get('/cartitem',controller.cartitem,(req,res)=>{  res.render('cartitem')}) 
+
+router.post('/cartitem',(req,res)=>{  res.render('cartitem')}) 
+
+router.get('/cartitemfailed',(req,res)=>{  res.render('cartitemfailed')}) 
+
+router.post('/purchaseItem',controller.purchaseItem)
+
+router.get('/profile',controller.profile,
+(req,res,next)=>{
+    if (req.data) { res.render("profile", 
+    { data: req.data });
+} else { res.redirect("/login"); }
+
 });
-
-router.get('/thanks',(req,res)=>{
-    res.render('thanks')
-});
-
-router.get('/loginok',(req,res)=>{
-    res.render('loginok')
-}) 
-
-router.get('/createaccfailed',(req,res)=>{
-    res.render('createaccfailed')
-}) 
-
-router.get('/loginfailed',(req,res)=>{
-    res.render('loginfailed')
-}) 
-
-router.post('/purchasing',controller.purchaseItem)
-
-
-
-router.get('/profile',controller.profile,(req,res,next)=>{
-    if (req.data) {
-        res.render("profile", { data: req.data });
-      } else {
-        res.redirect("/login");
-      }
-});
-
-
 
 module.exports = router;
+
