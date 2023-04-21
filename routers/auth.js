782479831e7562d5125cad15ebc7f7e9ca4f0fcb
Yogesh Waran P <118171405+yogeshwaran525@users.noreply.github.com>
2023-04-21 20:27:03 +0530

    Update auth.js

--- routers/auth.js
+++ routers/auth.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const controller = require("../controller/purchase");
 const router = express.Router();
-
-
-// router.post("/login", userController.login);
-
-
+// Form Login ,createaccount and Profile actions are carried out and post in page
 router.post("/register", controller.register);
 router.post("/login", controller.login);
 router.get("/profile", controller.login);
-
+router.get("/logout", controller.logout);
+router.post("/purchaseItem", controller.purchaseItem);
 
 module.exports = router;
