0daf221db4134cdb7d42b8cc381084a2e67ad906
Yogesh Waran P <118171405+yogeshwaran525@users.noreply.github.com>
2023-04-21 20:29:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,47 +7,27 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const app = express();
 const bodyParser = require('body-parser');  
-
 // Parsing middleware
 // app.use(express.urlencoded({ extended : true }));
 // app.use(express.json()); // New
-
-dotenv.config({
-    path: "./.env",
-  });
-
-
-  const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root123',
-    database: 'login_crud'
-})
-
-db.connect((error)=>{
-    if(!error){
-        console.log('Database Connection Done');
-    }else{
-        console.log('Database Not Connected');
-    }
-})
-
+// Password and some information store in .env file
+dotenv.config({ path: "./.env", });
 app.use(cookieParser());
-
 app.use(express.urlencoded({ extended : false}))
+app.use(express.json())
+// Static file Public folder
 const filesspath =path.join(__dirname,"./public");
 app.use(express.static(filesspath));
-
+// hbs view engine and html files in hbs file
 app.set('view engine','hbs');
-
+// Header and Footer static files all pages used in {{>header}} or {{>footer}}
 const partialspath = path.join(__dirname +"/views/partials");
 hbs.registerPartials(partialspath);
-
+// Pages File
 app.use('/',require('./pages/page'))
-
 // Redirect to Folder router file auth.js module . exports
 app.use("/auth",require("./routers/auth"))
-
+// Port Number Listening
 app.listen(port,()=>{
     console.log('Server is listening Port : ' +`${port}`);
 })
